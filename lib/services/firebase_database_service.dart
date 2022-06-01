93fc993ec6f29483cb446c4bea842534accf677a
nuilewis <lewisnuikweh@gmail.com>
2022-06-01 08:29:08 +0100

    added firebase database service

---
+++ lib/services/firebase_database_service.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseDBService {
+  final FirebaseDatabase _reference = FirebaseDatabase.instance;
+
+  Future<DatabaseEvent> getData(String ref) async {
+    DatabaseEvent value = await _reference.ref(ref).once();
+    //debugPrint("database value is ${value.snapshot.value}");
+
+    return value;
+  }
+
+  getStreamData(String ref) async {
+    //getting the stream
+    Stream<DatabaseEvent> valueStream = _reference.ref(ref).onValue;
+
+    //Subscribing to the stream
+    valueStream.listen((DatabaseEvent event) {
+      ///Dow aht ever you want with the stream
+      print("Event TYpe ${event.type}");
+      print("Event snapshot ${event.snapshot}");
+    });
+  }
+}
