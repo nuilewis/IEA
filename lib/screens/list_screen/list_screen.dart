a921bef675ad25b53c7f74cb1ae04cd510e7c90d
nuilewis <lewisnuikweh@gmail.com>
2022-06-18 14:18:46 +0100

    added list screen

---
+++ lib/screens/list_screen/list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../details_screen/components/side_menu.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      children: [
+        const Expanded(flex: 1, child: SideMenu()),
+        Expanded(flex: 10, child: Container()),
+      ],
+    ));
+  }
+}
