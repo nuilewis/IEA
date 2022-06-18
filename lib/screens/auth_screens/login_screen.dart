59c75de398b6059c1b9dcd0e6ed3ca418dd23529
nuilewis <lewisnuikweh@gmail.com>
2022-06-18 14:17:40 +0100

    added login and signup screen

---
+++ lib/screens/auth_screens/login_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:water_project/constants.dart';
+import 'package:water_project/global_components/button.dart';
+
+import '../details_screen/components/side_menu.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final Key passkey = GlobalKey();
+  final Key emailKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Form(
+        key: formKey,
+        child: Center(
+          child: SizedBox(
+            width: screenSize.width * .3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Log In",
+                  style: theme.textTheme.bodyText1,
+                ),
+                Text(
+                  "Email",
+                  style: theme.textTheme.bodyText1,
+                ),
+                const SizedBox(height: kDefaultPadding),
+                TextFormField(
+                  key: emailKey,
+                  controller: emailController,
+                  decoration:
+                      customTextFieldDecoration.copyWith(hintText: "email"),
+                  validator: (value) {
+                    if (!value!.contains("@")) {
+                      return "Please Input a valid email address";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: kDefaultPadding2x),
+                const Text("Password"),
+                const SizedBox(height: kDefaultPadding),
+                TextFormField(
+                  key: passkey,
+                  controller: passwordController,
+                  decoration:
+                      customTextFieldDecoration.copyWith(hintText: "Password"),
+                  validator: (value) {
+                    if (value!.length < 8) {
+                      return "Your password is too short";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: kDefaultPadding2x),
+                CustomButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      ///TODO: add methods to sign the user in
+
+                    }
+                  },
+                  text: "Login",
+                  textColor: Colors.white,
+                  bgColor: kPurple,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/auth_screens/signup_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+import '../../global_components/button.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Key passkey = GlobalKey();
+  final Key emailKey = GlobalKey();
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPassController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPassController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: SizedBox(
+            width: screenSize.width * .3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Register",
+                  style: theme.textTheme.bodyText1,
+                ),
+                Text(
+                  "Email",
+                  style: theme.textTheme.bodyText1,
+                ),
+                const SizedBox(height: kDefaultPadding),
+                TextFormField(
+                  key: emailKey,
+                  controller: emailController,
+                  decoration:
+                      customTextFieldDecoration.copyWith(hintText: "email"),
+                  validator: (value) {
+                    if (!value!.contains("@")) {
+                      return "Please Input a valid email address";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: kDefaultPadding2x),
+                const Text("Password"),
+                const SizedBox(height: kDefaultPadding),
+                TextFormField(
+                  key: passkey,
+                  controller: passwordController,
+                  decoration:
+                      customTextFieldDecoration.copyWith(hintText: "Password"),
+                  validator: (value) {
+                    if (value!.length < 8) {
+                      return "Your password is too short";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const Text("Confirm Password"),
+                const SizedBox(height: kDefaultPadding),
+                TextFormField(
+                  key: passkey,
+                  controller: confirmPassController,
+                  decoration:
+                      customTextFieldDecoration.copyWith(hintText: "Password"),
+                  validator: (value) {
+                    if (confirmPassController.text == passwordController.text) {
+                      return "Your password's don't match";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: kDefaultPadding2x),
+                CustomButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ///TODO: add methods to register the user
+
+                    }
+                  },
+                  text: "Login",
+                  textColor: Colors.white,
+                  bgColor: kPurple,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
