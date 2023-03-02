00625fa13f3def25f47a1232909bdb95363aa193
nuilewis <lewisnuikweh@gmail.com>
2023-03-02 17:16:31 +0100

    Implemented "Sensors" functionality

--- lib/providers/sensor_provider.dart
+++ lib/providers/sensor_provider.dart
@@ -1,10 +1,12 @@
 import 'package:dartz/dartz.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:water_project/models/sensor_model.dart';
 import 'package:water_project/repositories/sensor_repository.dart';
 
 import '../core/errors/failure.dart';
 import '../models/flow_data_model.dart';
+import '../models/project_model.dart';
 
 enum SensorState { initial, submitting, success, error }
 
@@ -18,9 +20,9 @@ class SensorProvider extends ChangeNotifier {
 
   SensorProvider({required this.sensorRepository});
 
-  Future<void> getSensors() async {
+  Future<void> getSensors({required Project project}) async {
     Either<Failure, List<Sensor>> response =
-        await sensorRepository.getSensorInformation();
+        await sensorRepository.getSensorInformation(project: project);
 
     response.fold((failure) {
       errorMessage = failure.errorMessage ??
@@ -35,15 +37,19 @@ class SensorProvider extends ChangeNotifier {
   }
 
   Future<void> getSensorData(Sensor sensor) async {
-    Either<Failure, List<FlowRate>> response =
+    Either<Failure, Stream<DatabaseEvent>> response =
         await sensorRepository.getSensorFlowRateData(sensor);
 
     response.fold((failure) {
       errorMessage = failure.errorMessage ??
           "An error occurred while getting the sensor flow rate data";
       state = SensorState.error;
-    }, (flowRate) {
-      flowRateData = flowRate;
+    }, (flowRateStream) {
+      flowRateStream.listen((event) {
+        ///Todo: find out how to extract both the velocity of water, and date time of the log from the stream
+        FlowRate flowRate = FlowRate(flowRate: event.snapshot.value as double);
+        flowRateData.add(flowRate);
+      });
       state = SensorState.success;
     });
 
--- lib/services/sensors/sensor_firestore_service.dart
+++ lib/services/sensors/sensor_firestore_service.dart
@@ -1,29 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:water_project/services/firebase_base_services.dart';
 
+import '../../models/project_model.dart';
 import '../../models/sensor_model.dart';
 
 class SensorFirestoreService extends FirestoreService {
-  Future<void> uploadSensorInfo({required Sensor sensor}) async {
+  Future<void> uploadSensorInfo(
+      {required Sensor sensor, required Project project}) async {
     Map<String, dynamic> sensorData = sensor.toMap();
 
     ///Todo: think hard about this databse schema
     await firestore
         .collection("project")
-        .doc("projectId")
+        .doc(project.id)
         .collection("sensor")
         .doc()
         .set(sensorData);
   }
 
-  Future<void> updateSensorInfo({required Sensor sensor}) async {
+  Future<void> updateSensorInfo(
+      {required Sensor sensor, required Project project}) async {
     Map<String, dynamic> sensorData = sensor.toMap();
 
     ///Todo: think hard about this databse schema
     await firestore
         .collection("project")
-        .doc("projectId")
+        .doc(project.id)
         .collection("sensor")
         .doc(sensor.id)
         .update(sensorData);
+  }
+
+  Future<QuerySnapshot> getSensorInfo({required Project project}) async {
+    ///Todo: think hard about this databse schema
+    return await firestore
+        .collection("project")
+        .doc(project.id)
+        .collection("sensor")
+        .get();
   }
 }
