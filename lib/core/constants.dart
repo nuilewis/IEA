0c4d8b8d51ff2039eb25bfdada945ff92ed97467
nuilewis <lewisnuikweh@gmail.com>
2024-01-15 19:32:52 +0100

    style: updated colors

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 ///-------DateTime Constants -------////
 
 Map<int, String> monthsOfYear = {
@@ -31,7 +30,7 @@ class AppInputDecoration {
         errorBorder: OutlineInputBorder(
           gapPadding: 4,
           borderSide:
-          BorderSide(color: Theme.of(context).colorScheme.error, width: 1),
+              BorderSide(color: Theme.of(context).colorScheme.error, width: 1),
           borderRadius: BorderRadius.circular(12),
         ),
         border: OutlineInputBorder(
@@ -58,7 +57,7 @@ class AppInputDecoration {
         isDense: true,
         fillColor: Theme.of(context).cardColor,
         hintStyle:
-        AppTextStyles.bodyMedium.copyWith(color: AppColours.neutral50),
+            AppTextStyles.bodyMedium.copyWith(color: AppColours.neutral50),
         labelStyle: AppTextStyles.bodyMedium.copyWith(color: AppColours.black));
   }
 }
@@ -70,20 +69,23 @@ class AppColours {
 
   ///-----------Purple---------///
   static const Color blueSeed = Color(0xFF1F84FB);
-  static const Color blue10 = Color(0xFF002F64);
-  static const Color blue20 = Color(0xFF002F64);
-  static const Color blue30 = Color(0xFF00458D);
-  static const Color blue40 = Color(0xFF005DB9);
-  static const Color blue50 = Color(0xFF0075E6);
-  static const Color blue60 = Color(0xFF4090FF);
-  static const Color blue70 = Color(0xFF7BACFF);
-  static const Color blue80 = Color(0xFFAAC7FF);
-  static const Color blue90 = Color(0xFFD6E3FF);
-  static const Color blue95 = Color(0xFFECF0FF);
+  static const Color blue5 = Color(0xFF000448);
+  static const Color blue10 = Color(0xFF000866);
+  static const Color blue20 = Color(0xFF0012A0);
+  static const Color blue30 = Color(0xFF1327D2);
+  static const Color blue40 = Color(0xFF3748E8);
+  static const Color blue50 = Color(0xFF5465FF);
+  static const Color blue60 = Color(0xFF7885FF);
+  static const Color blue70 = Color(0xFF9BA4FF);
+  static const Color blue80 = Color(0xFFBDC2FF);
+  static const Color blue90 = Color(0xFFDFE0FF);
+  static const Color blue95 = Color(0xFFF1EFFF);
+  static const Color blue98 = Color(0xFFFBF8FF);
   static const Color blue99 = Color(0xFFFDFBFF);
 
   ///-----------Green---------///
   static const Color greenSeed = Color(0xFF0DCC70);
+  static const Color green5 = Color(0xFF001507);
   static const Color green10 = Color(0xFF00210D);
   static const Color green20 = Color(0xFF00391B);
   static const Color green30 = Color(0xFF005229);
@@ -94,10 +96,12 @@ class AppColours {
   static const Color green80 = Color(0xFF3AE283);
   static const Color green90 = Color(0xFF5FFF9C);
   static const Color green95 = Color(0xFFC3FFCF);
+  static const Color green98 = Color(0xFFEAFFEA);
   static const Color green99 = Color(0xFFF5FFF3);
 
   ///-----------Yellow---------///
   static const Color yellowSeed = Color(0xFFFECB48);
+  static const Color yellow5 = Color(0xFF181000);
   static const Color yellow10 = Color(0xFF251A00);
   static const Color yellow20 = Color(0xFF3F2E00);
   static const Color yellow30 = Color(0xFF5A4300);
@@ -108,10 +112,12 @@ class AppColours {
   static const Color yellow80 = Color(0xFFF1C03D);
   static const Color yellow90 = Color(0xFFFFDF99);
   static const Color yellow95 = Color(0xFFFFEFD2);
+  static const Color yellow98 = Color(0xFFFFF8F2);
   static const Color yellow99 = Color(0xFFFFFBFF);
 
   ///-----------Red---------///
   static const Color redSeed = Color(0xFFFF4C4C);
+  static const Color red15 = Color(0xFF2D0002);
   static const Color red10 = Color(0xFF410004);
   static const Color red20 = Color(0xFF68000B);
   static const Color red30 = Color(0xFF930014);
@@ -122,19 +128,22 @@ class AppColours {
   static const Color red80 = Color(0xFFFFB3AE);
   static const Color red90 = Color(0xFFFFDAD7);
   static const Color red95 = Color(0xFFFFEDEB);
+  static const Color red98 = Color(0xFFFFF8F7);
   static const Color red99 = Color(0xFFFFFBFF);
 
   ///-----------Neutral---------///
-  static const Color neutral10 = Color(0xFF1A1B1E);
-  static const Color neutral20 = Color(0xFF313033);
-  static const Color neutral30 = Color(0xFF48464A);
-  static const Color neutral40 = Color(0xFF605D62);
-  static const Color neutral50 = Color(0xFF79767A);
-  static const Color neutral60 = Color(0xFF938F94);
-  static const Color neutral70 = Color(0xFFAEAAAF);
-  static const Color neutral80 = Color(0xFFCAC4CF);
-  static const Color neutral90 = Color(0xFFE6E1E6);
-  static const Color neutral95 = Color(0xFFF4EFF4);
+  static const Color neutral5 = Color(0xFF111114);
+  static const Color neutral10 = Color(0xFF1B1B1F);
+  static const Color neutral20 = Color(0xFF303034);
+  static const Color neutral30 = Color(0xFF47464A);
+  static const Color neutral40 = Color(0xFF5F5E62);
+  static const Color neutral50 = Color(0xFF78767A);
+  static const Color neutral60 = Color(0xFF929094);
+  static const Color neutral70 = Color(0xFFADAAAF);
+  static const Color neutral80 = Color(0xFFC8C5CA);
+  static const Color neutral90 = Color(0xFFE4E1E6);
+  static const Color neutral95 = Color(0xFFF3F0F4);
+  static const Color neutral98 = Color(0xFFFCF8FD);
   static const Color neutral99 = Color(0xFFFFFBFF);
 }
 
@@ -142,14 +151,11 @@ class AppColours {
 class AppTextStyles {
   ///---Display---//
 
-
   static TextStyle displayLarge = const TextStyle(
     fontWeight: FontWeight.normal,
     fontSize: 57,
     height: 1,
   );
-
-
 
   static TextStyle displayMedium = const TextStyle(
     fontWeight: FontWeight.normal,
@@ -157,14 +163,11 @@ class AppTextStyles {
     height: 1,
   );
 
-
   static TextStyle displaySmall = const TextStyle(
     fontWeight: FontWeight.normal,
     fontSize: 36,
     height: 1,
   );
-
-
 
   ///---Headline---///
   static TextStyle headlineLarge = const TextStyle(
